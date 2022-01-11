3bc1dfc05c9f62b70fff2563a4c55ed5677f6342
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-10 21:54:40 -0300

    Feat: Criando o ClientController

---
+++ src/app/controllers/ClientController.js
@@ -0,0 +1,104 @@
+const ClientsRepository = require('../repositories/ClientsRepository');
+
+class ClientController {
+  async index(request, response) {
+    const { orderBy } = request.query;
+
+    const clients = await ClientsRepository.findAll(orderBy);
+
+    response.status(200).json(clients);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const client = await ClientsRepository.findById(id);
+
+    if (!client) {
+      return response.status(404).json(
+        { message: 'No client found' },
+      );
+    }
+
+    response.status(200).json(client);
+  }
+
+  async store(request, response) {
+    const {
+      name, email, phone, password, store_id, plan_id,
+    } = request.body;
+
+    const clientByEmail = await ClientsRepository.findByEmail(email);
+
+    if (clientByEmail) {
+      return response.status(400).json({
+        error: 'Email já está sendo usado',
+      });
+    }
+
+    const newClient = await ClientsRepository.create({
+      name, email, phone, password, store_id, plan_id,
+    });
+
+    return response.status(200).json(
+      { message: `${newClient.name}, criou a sua conta` },
+    );
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, password, store_id, plan_id,
+    } = request.body;
+
+    const clientExist = await ClientsRepository.findById(id);
+
+    if (!clientExist) {
+      return response.status(404).json({
+        error: 'Client not found',
+      });
+    }
+
+    if (!email) {
+      return response.status(400).json({
+        error: 'Email is required',
+      });
+    }
+
+    const clientByEmail = await ClientsRepository.findByEmail(email);
+
+    if (clientByEmail && clientByEmail.id !== id) {
+      return response.status(400).json({
+        error: 'Email is already in use',
+      });
+    }
+
+    const updatedClient = await ClientsRepository.update(id, {
+      name, email, phone, password, store_id, plan_id,
+    });
+
+    response.status(200).json({
+      message: `Cliente ${updatedClient.name} foi atualizado`,
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const clientExist = await ClientsRepository.findById(id);
+
+    if (!clientExist) {
+      return response.status(404).json(
+        { message: 'Cliente não encontrado' },
+      );
+    }
+
+    const client = await ClientsRepository.delete(id);
+
+    response.status(200).json({
+      message: `${client.name} foi excluído`,
+    });
+  }
+}
+
+module.exports = new ClientController();
